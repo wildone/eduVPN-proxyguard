eb41a4f3a9f93ff7be14d81e3e54f8d53d636903
jwijenbergh <jeroenwijenbergh@protonmail.com>
2024-02-13 15:59:39 +0100

    Prepare release: Fix unstaged changes check

--- prepare_release.sh
+++ prepare_release.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "$1" ] && { echo "no version supplied"; exit 1; }
 
 # check if there are unstaged changes
-[ -z "$(git diff --quiet)" ] && { echo "there are unstaged changes, commit them first"; exit 1; }
+[ -n "$(git status -s)" ] && { echo "there are unstaged changes, commit them first"; exit 1; }
 
 # lint
 make lint
@@ -14,7 +14,7 @@ make lint
 make fmt
 
 # commit
-[ -z "$(git diff --quiet)" ] && { git add -u; git commit -m "Format: Run gofumpt"; }
+[ -n "$(git status -s)" ] && { git add -u; git commit -m "Format: Run gofumpt"; }
 
 # update version
 sed -i "s/const version = \".*\"/const version = \"$1\"/" version.go
